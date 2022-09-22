851a8af9f2e663650bd77f94d7eb2f0721ed387c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-22 11:18:30 -0500

    optimistic ui (remix is good here!)

--- remix-app/app/features/clocks/clock-list.tsx
+++ remix-app/app/features/clocks/clock-list.tsx
@@ -1,10 +1,29 @@
-import { Form } from "@remix-run/react"
+import { Form, useTransition } from "@remix-run/react"
 import { Plus } from "react-feather"
 import { solidButton } from "~/ui/styles"
 import { Clock } from "./clock"
 import type { ClockState } from "./clock-state"
 
-export function ClockList({ clocks }: { clocks: ClockState[] }) {
+export function ClockList(props: { clocks: ClockState[] }) {
+  const transition = useTransition()
+
+  const deletedClockId =
+    transition.submission?.action === "/clocks" &&
+    transition.submission?.method === "DELETE" &&
+    transition.submission.formData.get("id")
+
+  let clocks = props.clocks.filter((clock) => clock.id !== deletedClockId)
+
+  if (
+    transition.submission?.action === "/clocks" &&
+    transition.submission?.method === "POST"
+  ) {
+    clocks = [
+      ...clocks,
+      { id: "placeholder", name: "New Clock", progress: 0, maxProgress: 4 },
+    ]
+  }
+
   return (
     <div className="grid gap-4">
       {clocks.length > 0 && (
