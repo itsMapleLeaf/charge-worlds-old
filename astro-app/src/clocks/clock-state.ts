2513341dd18c46aac4485c40c17600febdf4baaa
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-22 15:11:33 -0500

    we remix

--- astro-app/src/clocks/clock-state.ts
+++
@@ -1,6 +0,0 @@
-export type ClockState = {
-  id: string
-  name: string
-  progress: number
-  maxProgress: number
-}
--- astro-app/src/common/array-replace-by.ts
+++
@@ -1,5 +0,0 @@
-export const arrayReplaceBy = <T>(
-  items: T[],
-  replacement: T,
-  predicate: (item: T) => boolean,
-) => items.map((item) => (predicate(item) ? replacement : item))
