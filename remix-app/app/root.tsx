b2647c6e1234d000b29e5cdc2970145c67f9e3c6
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-22 10:12:03 -0500

    add metadata

--- remix-app/app/root.tsx
+++ remix-app/app/root.tsx
@@ -9,11 +9,27 @@ import {
 } from "@remix-run/react"
 import tailwind from "./generated/tailwind.css"
 
+const appName = "Charge Worlds"
+const appDescription = "Virtual environment for the Charge RPG system"
+const appUrl = "https://charge.mapleleaf.dev"
+
 export const meta: MetaFunction = () => ({
   // eslint-disable-next-line unicorn/text-encoding-identifier-case
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+  "charset": "utf-8",
+  "viewport": "width=device-width,initial-scale=1",
+
+  "title": appName,
+  "description": appDescription,
+
+  "og:type": "website",
+  "og:url": appUrl,
+  "og:title": appName,
+  "og:description": appDescription,
+
+  "twitter:card": "summary_large_image",
+  "twitter:url": appUrl,
+  "twitter:title": appName,
+  "twitter:description": appDescription,
 })
 
 export const links: LinksFunction = () => [
