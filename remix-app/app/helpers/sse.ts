c6d969e765a5c7e3df56ce8dd850bd91cb553de4
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-22 15:07:52 -0500

    improve typing for SSE stuff

--- remix-app/app/helpers/sse.ts
+++ remix-app/app/helpers/sse.ts
@@ -1,6 +1,13 @@
+import type { DataFunctionArgs, SerializeFrom } from "@remix-run/node"
+import { useEffect, useState } from "react"
+import { useEvent } from "./react"
+import type { MaybePromise } from "./types"
+
 export type Unsubscribe = () => void
 
-export function sse<T>(
+export type ServerEventsResponse<T> = Response & { __serverEvent: T }
+
+export function serverEvents<T>(
   request: Request,
   init: (emit: (event: T) => void) => Unsubscribe | undefined | void,
 ) {
@@ -20,5 +27,36 @@ export function sse<T>(
       "Content-Type": "text/event-stream",
       "Cache-Control": "no-cache",
     },
-  })
+  }) as ServerEventsResponse<T>
+}
+
+export type AnyServerEventsLoader = (
+  args: DataFunctionArgs,
+) => MaybePromise<ServerEventsResponse<unknown>>
+
+export type EventSourceData<F extends AnyServerEventsLoader> = SerializeFrom<
+  Awaited<ReturnType<F>>["__serverEvent"]
+>
+
+export function useEventSource<F extends AnyServerEventsLoader>(
+  path: string,
+  callback: (data: EventSourceData<F>) => void,
+) {
+  const handleMessage = useEvent((event: MessageEvent) =>
+    callback(JSON.parse(event.data)),
+  )
+  useEffect(() => {
+    const source = new EventSource(path)
+    source.addEventListener("message", handleMessage)
+    return () => source.close()
+  }, [path, handleMessage])
+}
+
+export function useEventSourceState<F extends AnyServerEventsLoader>(
+  path: string,
+  initialState: EventSourceData<F>,
+) {
+  const [state, setState] = useState(initialState)
+  useEventSource(path, setState)
+  return state
 }
