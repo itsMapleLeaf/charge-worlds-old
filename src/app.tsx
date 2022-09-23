681a9f73a342b48ab852a5e4854ae824c9383e96
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-22 20:25:52 -0500

    rendering clock data

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react"
+import type { ClockState } from "./features/clocks/clock-state"
+import { proxyClient } from "./trpc/client"
 import { trpc } from "./trpc/react"
 
+const userId = crypto.randomUUID()
+
+const initialClocks = await proxyClient.clocks.list.query()
+
 export function App() {
-  const { data } = trpc.message.useQuery()
+  const [clocks, setClocks] = useState<ClockState[]>(initialClocks)
+
+  trpc.clocks.listUpdated.useSubscription(undefined, {
+    onData: (event) => {
+      if (event.userId === userId) return
+      setClocks(event.clocks)
+    },
+  })
+
   return (
     <div>
       <h1>ayy lmao</h1>
-      <p>{data}</p>
+      <p>{JSON.stringify(clocks)}</p>
     </div>
   )
 }
--- src/features/clocks/clock-router.ts
+++ src/features/clocks/clock-router.ts
@@ -1,3 +1,24 @@
-import { t } from "../../trpc/app-router"
+import { observable } from "@trpc/server/observable"
+import { Emitter } from "../../helpers/emitter"
+import { t } from "../../trpc/init"
+import { getClocks } from "./clock-actions"
+import type { ClockState } from "./clock-state"
 
-export const clockRouter = t.router({})
+type ClockListEvent = {
+  clocks: ClockState[]
+  userId?: string
+}
+
+const events = new Emitter<ClockListEvent>()
+
+export const clockRouter = t.router({
+  list: t.procedure.query(() => getClocks()),
+
+  listUpdated: t.procedure.subscription(async () => {
+    const clocks = await getClocks()
+    return observable<ClockListEvent>((emit) => {
+      emit.next({ clocks })
+      return events.subscribe(emit.next)
+    })
+  }),
+})
