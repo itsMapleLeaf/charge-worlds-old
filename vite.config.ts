d4b800533f1308b4023153082df841ab20bc6366
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-23 12:24:04 -0500

    use preact compat

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import react from "@vitejs/plugin-react"
+import preact from "@preact/preset-vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [preact()],
+  resolve: {
+    alias: {
+      "react": "preact/compat",
+      "react-dom": "preact/compat",
+    },
+  },
   build: {
     sourcemap: true,
   },
