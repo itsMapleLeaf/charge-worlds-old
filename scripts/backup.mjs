ca77b8aa945a22faf0b0d0ad6dd4a3c3c4a93304
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-25 14:21:07 -0500

    add backup cron job

---
+++ scripts/backup.mjs
@@ -0,0 +1,28 @@
+import { createClient } from "@supabase/supabase-js"
+import fetch from "cross-fetch"
+import cron from "node-cron"
+
+const supabase = createClient(
+  // @ts-expect-error
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY,
+)
+
+cron.schedule(`0 1 * * *`, async () => {
+  const data = await fetch(
+    "https://api.liveblocks.io/v2/rooms/{roomId}/storage",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.LIVEBLOCKS_SECRET_KEY}`,
+      },
+    },
+  ).then((res) => res.json())
+
+  const { error } = await supabase.storage
+    .from("liveblocks-backup")
+    .upload(`backup-${Date.now()}.json`, JSON.stringify(data))
+
+  if (error) {
+    console.error("Supabase upload failed:", error)
+  }
+})
