29b0758f6cbc642bba70a1fc383c36b08a801dcf
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-25 03:39:34 -0500

    remove other remaining astro things

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,6 @@ module.exports = {
   extends: [
     require.resolve("@itsmapleleaf/configs/eslint"),
     "plugin:tailwindcss/recommended",
-    "plugin:astro/recommended",
   ],
   ignorePatterns: [
     "**/node_modules/**",
@@ -18,21 +17,7 @@ module.exports = {
   ],
   parserOptions: {
     project: require.resolve("./tsconfig.json"),
-    extraFileExtensions: [".astro"],
   },
-  overrides: [
-    {
-      files: ["**/*.astro"],
-      parser: "astro-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
-      },
-      rules: {
-        "react/no-unknown-property": "off",
-      },
-    },
-  ],
   settings: {
     tailwindcss: {
       whitelist: ["font-body", "font-header"],
