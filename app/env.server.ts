2a7d437a9315beddc62f979fb3c6188b8aa01855
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-10-11 11:40:55 -0500

    remove old allowlist stuff

--- app/env.server.ts
+++ app/env.server.ts
@@ -5,17 +5,20 @@ const envSchema = z.object({
     .enum(["development", "production", "test"])
     .optional()
     .default("development"),
+
+  DATABASE_URL: z.string(),
   COOKIE_SECRET: z.string(),
+
   LIVEBLOCKS_SECRET_KEY: z.string(),
   LIVEBLOCKS_PUBLIC_KEY: z.string(),
+
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
   DISCORD_REDIRECT_URI: z.string(),
-  DISCORD_ALLOWLIST: z.string(),
+
   SUPABASE_URL: z.string(),
   SUPABASE_SERVICE_KEY: z.string(),
-  ADMIN_DISCORD_ID: z.string(),
-  DATABASE_URL: z.string(),
+
   PUSHER_APP_ID: z.string(),
   PUSHER_KEY: z.string(),
   PUSHER_SECRET: z.string(),
